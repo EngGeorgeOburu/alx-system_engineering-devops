f01d20f3544e58afe2952d62a27b6c1e2c29b999
root <oburugeorge94@gmail.com>
2024-04-24 06:25:36 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This manifest creates a file at /tmp
 file { '/tmp/school':
-ensure => '/tmp/School',
-owner => 'www-data',
-group => 'www-data',
-mode => '0743',
-content => 'I love Puppet',
+  ensure => '/tmp/School',
+  owner => 'www-data',
+  group => 'www-data',
+  mode => '0743',
+  content => 'I love Puppet',
 }
